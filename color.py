8d3f27ac4bf6f23aaf2f49e122b9d8b090aea30c
Abhijith0369 <prakashabhijith408@gmail.com>
2025-11-05 14:54:40 +0530

    Add files via upload

---
+++ color.py
@@ -0,0 +1,5 @@
+list1 =["Red","green","Blue","Yellow"]
+list2 =["Green","Yelllow","Black"]
+for color in list1:
+    if color  not in list2:
+         print(color)
---
+++ litofintegers.py
@@ -0,0 +1,8 @@
+numbers =[10,15,20,25,20]
+result=[]
+
+for n in numbers:
+    if n%2!=0:
+        result.append(n)
+print(result)
+   
